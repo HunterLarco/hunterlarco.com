39f322475288a02b0cd8e0e21420e11c80ca92da
Hunter John Larco <admin@larcolabs.com>
2016-10-22 17:48:46 -0700

    Installed: Sass, Gulp, Npm, App Engine Module

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import webapp2
 import os
 
 
-class MainHandler(webapp2.RequestHandler):
+class DefaultHandler(webapp2.RequestHandler):
   def get(self):
     template_values = {}
-    path = os.path.join(os.path.dirname(__file__), 'main.html')
+    path = os.path.join(os.path.dirname(__file__), 'templates/landing.html')
     self.response.out.write(template.render(path, template_values))
 
 
 app = webapp2.WSGIApplication([
-  ('/.*', MainHandler)
-], debug=True)
+  ('.*', DefaultHandler)
+])
---
+++ appengine_config.py
@@ -0,0 +1,4 @@
+from google.appengine.ext import vendor
+
+# Add any libraries installed in the "vendor" folder.
+vendor.add('vendor')
