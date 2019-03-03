0cee8ab542b10d36b53d3c0a138472aa95c65092
hunterlarco <hunter@larcolabs.com>
2019-03-03 09:38:52 -0500

    Remove postcss-sorting due to missing support for VueJS templates.

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,7 @@
 module.exports = {
   plugins: {
-    'autoprefixer': {
-      browsers: [
-        "> 1%",
-        "last 2 versions",
-        "not ie <= 8",
-      ],
-    },
-    'postcss-sorting': {
-      'properties-order': 'alphabetical',
+    autoprefixer: {
+      browsers: ['> 1%', 'last 2 versions', 'not ie <= 8'],
     },
   },
-}
+};
