a33c4ec7b4eceea9d8b471badc2465e4230500c3
Hunter Larco <hunter@larcolabs.com>
2020-05-29 16:44:56 +0000

    hunter. visionary. artiste.

--- src/components/HunterLarco.vue
+++ src/components/HunterLarco.vue
@@ -1,19 +1,60 @@
 <template>
   <div :class="$style.Host">
     <div :class="$style.Text_Secondary">
-      <div :class="$style.Camera_Secondary">Hunter.</div>
+      <div :class="$style.Camera_Secondary" :style="cameras_.secondary.style">Artiste.</div>
     </div>
     <div :class="$style.Text_Tertiary">
-      <div :class="$style.Camera_Tertiary">Hunter.</div>
+      <div :class="$style.Camera_Tertiary" :style="cameras_.tertiary.style">Hunter.</div>
     </div>
     <div :class="$style.Text_Primary">
-      <div :class="$style.Camera_Primary">Hunter.</div>
+      <div :class="$style.Camera_Primary">Visionary.</div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      cameras_: {
+        secondary: {
+          style: '',
+        },
+        tertiary: {
+          style: '',
+        },
+      },
+    };
+  },
+
+  methods: {
+    mousemove_(event) {
+      const { clientX: x, clientY: y } = event;
+      const rx = x / window.innerWidth;
+      const ry = y / window.innerHeight;
+      const distance = Math.sqrt(Math.pow(rx, 2) + Math.pow(ry - 1, 2));
+      const range = Math.max(0.8 - distance, 0) / 0.8;
+
+      const angle = Math.atan2(ry - 0.5, rx - 0.5);
+
+      const offset = 100 + 150 * range;
+      this.cameras_.secondary.style = `transform: translate(${offset * Math.cos(angle)}px, ${
+        offset * Math.sin(angle)
+      }px)`;
+      this.cameras_.tertiary.style = `transform: translate(${-offset * Math.cos(angle)}px, ${
+        -offset * Math.sin(angle)
+      }px)`;
+    },
+  },
+
+  mounted() {
+    window.addEventListener('mousemove', this.mousemove_);
+  },
+
+  beforeDestroy() {
+    window.removeEventListener('mousemove', this.mousemove_);
+  },
+};
 </script>
 
 <style module lang="sass">
