7355f682ece41aa9bfabab9c1ee842f20bd31af0
Hunter Larco <hunter@larcolabs.com>
2020-05-29 16:21:38 +0000

    hero css

--- src/components/HunterLarco.vue
+++ src/components/HunterLarco.vue
@@ -1,60 +1,69 @@
 <template>
   <div :class="$style.Host">
-    <canvas :class="$style.Canvas" ref="canvas"></canvas>
+    <div :class="$style.Text_Secondary">
+      <div :class="$style.Camera_Secondary">Hunter.</div>
+    </div>
+    <div :class="$style.Text_Tertiary">
+      <div :class="$style.Camera_Tertiary">Hunter.</div>
+    </div>
+    <div :class="$style.Text_Primary">
+      <div :class="$style.Camera_Primary">Hunter.</div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      stop_: false,
-      ctx_: null,
-    };
-  },
-
-  methods: {
-    _resize() {
-      this.$refs.canvas.width = devicePixelRatio * this.$el.offsetWidth;
-      this.$refs.canvas.height = devicePixelRatio * this.$el.offsetHeight;
-    },
-
-    _loop() {
-      if (this.stop_) {
-        this.stop_ = false;
-      } else {
-        requestAnimationFrame(this._loop);
-      }
-
-      const { width, height } = this.$refs.canvas;
-      this.ctx_.clearRect(0, 0, width, height);
-    },
-  },
-
-  mounted() {
-    this.ctx_ = this.$refs.canvas.getContext('2d');
-
-    window.addEventListener('resize', this._resize);
-    this._resize();
-
-    this._loop();
-  },
-
-  beforeDestroy() {
-    window.removeEventListener('resize', this._resize);
-
-    this.stop_ = true;
-  },
-};
+export default {};
 </script>
 
 <style module lang="sass">
+@import '@/src/scss/fonts';
+
 .Host {
   overflow: hidden;
+  position: relative;
 }
 
-.Canvas {
-  height: 100%;
-  width: 100%;
+@mixin _text-base {
+  @include fonts-hero;
+
+  display: inline-block;
+  left: 50%;
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.Text_Primary {
+  @include _text-base;
+}
+
+.Text_Secondary {
+  @include _text-base;
+}
+
+.Text_Tertiary {
+  @include _text-base;
+}
+
+@mixin _camera-base {
+}
+
+.Camera_Primary {
+  @include _camera-base;
+}
+
+.Camera_Secondary {
+  @include _camera-base;
+
+  color: #676767;
+  transform: translate(-100px, -100px);
+}
+
+.Camera_Tertiary {
+  @include _camera-base;
+
+  color: #AFAFAF;
+  transform: translate(100px, 100px);
 }
 </style>
