9c331bf98bf003ffff6c019b8c592c555ea5b874
Hunter Larco <hunter@larcolabs.com>
2020-05-29 15:40:04 +0000

    navigation idea

---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="$style.Host">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style module lang="sass">
+@import '@/src/scss/fonts';
+
+.Host {
+  & > * {
+    @include fonts-navbar;
+  }
+}
+</style>
--- src/components/Pages/Home.vue
+++ src/components/Pages/Home.vue
@@ -1,12 +1,54 @@
 <template>
-  <div :class="$style.Host"></div>
+  <div :class="$style.Host">
+    <div :class="$style.Fold">
+      <div :class="$style.Role">Founder &amp; Engineer</div>
+      <div :class="$style.Hero" />
+      <NavBar :class="$style.NavBar">
+        <div :class="$style.NavBar_Item">About</div>
+        <div :class="$style.NavBar_Item">Work</div>
+        <div :class="$style.NavBar_Item">Contact</div>
+      </NavBar>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import NavBar from '@/src/components/NavBar';
+
+export default {
+  components: { NavBar },
+};
 </script>
 
 <style module lang="sass">
+@import '@/src/scss/fonts';
+@import '@/src/scss/layout';
+
 .Host {
+}
+
+.Fold {
+  @include layout-vertical;
+
+  height: 100vh;
+}
+
+.Role {
+  @include fonts-navbar;
+
+  flex-shrink: 0;
+  padding: 35px 100px;
+}
+
+.Hero {
+  flex-grow: 1;
+}
+
+.NavBar {
+  flex-shrink: 0;
+  padding: 35px 100px;
+}
+
+.NavBar_Item {
 }
 </style>
