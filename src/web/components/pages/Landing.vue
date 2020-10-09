1d71d15b0da5401ee92a6b35c0f5d872b2d57228
Hunter Larco <hunter@larcolabs.com>
2020-10-09 01:35:47 -0400

    recreate the existing website in the new vue + webpack build (#2)

--- src/web/components/pages/Landing.vue
+++ src/web/components/pages/Landing.vue
@@ -1,5 +1,22 @@
 <template>
-  <div :class="$style.Host"></div>
+  <div :class="$style.Host">
+    <div :class="$style.Text">
+      <p>
+        <b>Hunter Larco</b>
+        &mdash; Currently building a better home for art on the web
+        <a href="https://terrace.co" target="blank">@terrace.co</a>
+      </p>
+      <br />
+      <p>
+        <a href="https://www.linkedin.com/in/hunterlarco" target="blank"
+          >linkedin.com/in/hunterlarco</a
+        ><br />
+        <a href="http://github.com/hunterlarco" target="blank"
+          >github.com/hunterlarco</a
+        >
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,6 +24,24 @@ export default {};
 </script>
 
 <style module lang="sass">
+@import '@/src/web/sass/fonts';
+@import '@/src/web/sass/layout';
+
 .Host {
+  @include layout-center;
+  @include layout-fill;
+}
+
+.Text {
+  @include fonts-body;
+
+  & a {
+    color: #E71A8B;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 }
 </style>
