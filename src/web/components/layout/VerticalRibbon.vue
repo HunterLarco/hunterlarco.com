b140204964f625e97b9e7f6c93df4be80fb1ac6b
hunterlarco <hunter@larcolabs.com>
2020-10-12 16:09:34 -0400

    begin to ideate what a case study looks like

---
+++ src/web/components/layout/VerticalRibbon.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  props: {
+    maxWidth: {
+      type: String,
+      default: '100%',
+    },
+
+    centered: {
+      type: Boolean,
+      default: false,
+    },
+
+    padding: {
+      type: String,
+      default: null,
+    },
+  },
+
+  render(createElement) {
+    const style = { maxWidth: this.maxWidth };
+
+    if (this.centered) {
+      style.marginLeft = 'auto';
+      style.marginRight = 'auto';
+    }
+
+    if (this.padding) {
+      style.padding = this.padding;
+    }
+
+    return createElement('div', { style }, this.$slots.default);
+  },
+};
+</script>
