4dc361f4f4af8efbfcd508af00abb8d3ed6e4d00
hunterlarco <hunter@larcolabs.com>
2021-01-08 17:49:36 -0500

    mouse

--- src/web/components/pages/lab/Tink.vue
+++ src/web/components/pages/lab/Tink.vue
@@ -20,7 +20,7 @@ export default {
       options: {
         width: 800,
         height: 600,
-        showAngleIndicator: true,
+        //      showAngleIndicator: true,
       },
     });
 
@@ -28,12 +28,29 @@ export default {
     Matter.Runner.run(runner, engine);
 
     Matter.World.add(engine.world, [
-      Matter.Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
-      Matter.Bodies.rectangle(50, 40, 40, 40, {
-        angle: -0.2,
+      Matter.Bodies.rectangle(400, 600, 800, 1, { isStatic: true }),
+      Matter.Bodies.rectangle(50, 80, 40, 40, {
+        angle: -0.25,
         chamfer: { radius: 10 },
       }),
+      Matter.Bodies.rectangle(50, 10, 30, 30, {
+        angle: 0.1,
+        chamfer: { radius: 8 },
+      }),
     ]);
+
+    const mouse = Matter.Mouse.create(render.canvas);
+    const mouseConstraint = Matter.MouseConstraint.create(engine, {
+      mouse,
+      constraint: {
+        stiffness: 0.2,
+        render: {
+          visible: false,
+        },
+      },
+    });
+
+    Matter.World.add(engine.world, mouseConstraint);
 
     Matter.Render.run(render);
   },
