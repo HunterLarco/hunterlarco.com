2c45c526e51d40faed6d366ac5a4072980297040
hunterlarco <hunter@larcolabs.com>
2021-01-08 18:16:43 -0500

    circular bounds

--- src/web/components/pages/lab/Tink.vue
+++ src/web/components/pages/lab/Tink.vue
@@ -7,6 +7,44 @@
 <script>
 import * as Matter from 'matter-js';
 
+function createBoundingCircle(x, y, radius) {
+  const numParts = 100;
+  const arcAngle = (2 * Math.PI) / numParts;
+
+  const parts = [];
+  for (let i = 0; i < numParts; ++i) {
+    const p1 = {
+      x: radius * Math.cos(arcAngle * i),
+      y: radius * Math.sin(arcAngle * i),
+    };
+
+    const p2 = {
+      x: radius * Math.cos(arcAngle * i + arcAngle),
+      y: radius * Math.sin(arcAngle * i + arcAngle),
+    };
+
+    const center = {
+      x: (p1.x + p2.x) / 2,
+      y: (p1.y + p2.y) / 2,
+    };
+
+    const distance = Math.sqrt(
+      Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2)
+    );
+
+    const rectangle = Matter.Bodies.rectangle(center.x, center.y, distance, 1, {
+      isStatic: true,
+      angle: arcAngle * i + arcAngle / 2 + Math.PI / 2,
+    });
+
+    parts.push(rectangle);
+  }
+
+  const body = Matter.Body.create({ parts, isStatic: true });
+  Matter.Body.setPosition(body, { x, y });
+  return body;
+}
+
 export default {
   data() {
     return {};
@@ -28,17 +66,10 @@ export default {
     const runner = Matter.Runner.create();
     Matter.Runner.run(runner, engine);
 
-    Matter.World.add(engine.world, [
-      Matter.Bodies.rectangle(400, 600, 800, 1, { isStatic: true }),
-      Matter.Bodies.rectangle(50, 80, 40, 40, {
-        angle: -0.25,
-        chamfer: { radius: 10 },
-      }),
-      Matter.Bodies.rectangle(50, 10, 30, 30, {
-        angle: 0.1,
-        chamfer: { radius: 8 },
-      }),
-    ]);
+    const bounds = createBoundingCircle(400, 300, 200);
+    const rectangle = Matter.Bodies.rectangle(500, 200, 20, 20);
+
+    Matter.World.add(engine.world, [bounds, rectangle]);
 
     const mouse = Matter.Mouse.create(render.canvas);
     const mouseConstraint = Matter.MouseConstraint.create(engine, {
