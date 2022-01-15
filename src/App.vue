601317b73f7063df1057bf43050d59144339980d
Hunter Larco <hunter@larcolabs.com>
2022-01-15 14:13:20 -0500

    install sass

--- src/App.vue
+++ src/App.vue
@@ -11,3 +11,30 @@
   color: #2c3e50;
 }
 </style>
+
+<style lang="scss">
+@import "./styles/fonts.scss";
+@import "./styles/layout.scss";
+
+#app {
+  @include layout-fill;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+* {
+  color: inherit;
+  font: inherit;
+  margin: 0;
+  padding: 0;
+}
+
+html body {
+  @include fonts-clear;
+}
+
+[hidden] {
+  display: none !important;
+}
+</style>
