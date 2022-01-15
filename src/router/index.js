43112ab381f2a3244aac15d4cfc3493f985f8361
Hunter Larco <hunter@larcolabs.com>
2022-01-15 14:05:06 -0500

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    component: () => import('../views/Home.vue'),
+    meta: { title: 'Hunter Larco' },
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+    next();
+    if (to.meta.title) {
+          document.title =
+              typeof to.meta.title == 'function'
+              ? to.meta.title(to.params)
+              : to.meta.title;
+        }
+});
 
 export default router
